38498366ef7fdc688a34018e335409bbb962b5d5
JacobSidhu <jacobsidhu100@gmail.com>
2023-11-26 01:05:15 +0000

    flutter lab exercises file added

--- myflutter/lib/main.dart
+++ myflutter/lib/main.dart
@@ -1,43 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const DemoApp());
+void main() => runApp(const MyApp());
 
-class DemoApp extends StatelessWidget {
-  const DemoApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'hello world',
-        home: SafeArea(
-            child: Scaffold(
-          appBar: AppBar(
-              title: Text(
-                "Corn Hub",
-                style: TextStyle(color: Colors.black),
+  Widget build(BuildContext context) => MaterialApp(
+          home: SafeArea(
+              child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text("DemoApp"),
+          leading: Icon(Icons.menu),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.notification_add)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.access_alarms)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.search))
+          ],
+        ),
+        body: Column(
+          children: [
+            Flexible(
+              child: TextButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("hello i am here !"),
+                    duration: Duration(seconds: 3),
+                  ));
+                },
+                child: Text("Show SnakBar"),
               ),
-              leading: const Icon(Icons.menu),
-              backgroundColor: Color.fromARGB(255, 218, 200, 0),
-              actions: [
-                IconButton(
-                    onPressed: () {}, icon: const Icon(Icons.notification_add))
-              ]),
-          body: Center(
-            child: Image.asset(
-              'assests/images/download.png',
-              width: 360,
-              height: 360,
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            tooltip: "floating button",
-            elevation: 2,
-            foregroundColor: Color.fromARGB(255, 0, 0, 0),
-            backgroundColor: Color.fromARGB(255, 255, 213, 0),
-            child: Icon(Icons.navigation_sharp),
-          ),
-          backgroundColor: const Color.fromARGB(255, 57, 57, 57),
-        )));
-  }
+            )
+          ],
+        ),
+      )));
 }
